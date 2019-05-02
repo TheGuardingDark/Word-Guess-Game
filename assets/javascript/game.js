a1e465ad528c98df7f0d13b722f4359d0be804d6
Scout <therearespidersinthisemail@gmail.com>
2019-05-01 19:42:13 -0600

    added condition to distinguish between keystroke to start game and keystroke to guess letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,41 @@
 
-// var wins = 0;
-// var losses = 0;
-// var livesRemain = 12;
-// var filmPoster;
-// var film;
+
+
 // var isFirstGame = true;
-// var gameStarted = false;
+var gameStarted = false;
+var wins = 0;
+var losses = 0;
+var livesRemaining = 12;
+var filmPoster;
 
-
-// var filmPoster = document.getElementById("filmPoster")[0];
-// var winsDiv = document.getElementById("wins")[0];
-// var lossesDiv = document.getElementById("losses")[0];
-// var livesRemainDiv = document.getElementById("livesRemain")[0];
-// var messageDiv = document.getElementById("message")[0];
-// var userGuessesDiv = document.getElementById("userGuesses")[0];
-// var userInterface = document.getElementById("userInterface")[0];
-// var gameWrapper = document.getElementById("gameWrapper")[0];
-
-
-var  films = ["Slither", "The Omen", "The Ring", "The Orphanage", "Halloween", "The Exorcist", "Trick r Treat", "Phantasm", "The Evil Dead", "Cube", "Hellraiser", "Candyman", "Scream", "Saw", "Us", "IT", "Pet Sematary", "Audition", "The Others", "The Changeling", "The Conjuring", "Poltergeist", "Hausu", "The Shining", "Let the Right One In", "Suspiria", "Cannibal Holocaust", "The Woman in Black", "The Awakening"];
+var films = ["Slither", "The Omen", "The Ring", "The Orphanage", "Halloween", "The Exorcist", "Trick r Treat", "Phantasm", "The Evil Dead", "Cube", "Hellraiser", "Candyman", "Scream", "Saw", "Us", "IT", "Pet Sematary", "Audition", "The Others", "The Changeling", "The Conjuring", "Poltergeist", "Hausu", "The Shining", "Let the Right One In", "Suspiria", "Cannibal Holocaust", "The Woman in Black", "The Awakening"];
 var film = [];
 var alphabet = Array.from("abcdefghijklmnopqrstuvwxyz");
 var wrongGuesses = [];
 
+var winsText = document.getElementById("wins");
+var lossesText = document.getElementById("losses");
+var livesRemainText = document.getElementById("lives");
+var userGuessText = document.getElementById("userGuess");
+var holderText = document.getElementById("holder");
+
 
 document.onkeyup = function(event) {
 
+    if (gameStarted === false) {
+        
+    // isFirstGame = false;
+    gameStarted = true;
+    livesRemaining = 12;
+    randomFilm = [];
+    upperCaseFilm = [];
+    currentFilm = [];
+    wrongGuesses = [];
+    
+
     var guessFilmText = document.getElementById("guessFilm");
     
-    var userGuess = event.key.toUpperCase();
+    // var userGuess = event.key.toUpperCase();
 
     var randomFilm = films[Math.floor(Math.random() * films.length)];
 
@@ -44,9 +51,9 @@ document.onkeyup = function(event) {
 
     } else {
         film.push("_"); 
-           
+    }};
 
-}};
+
 
     guessFilmText.textContent = film;
 
@@ -55,4 +62,10 @@ document.onkeyup = function(event) {
 
     // console.log(film);
     // film.textContent = film;
+    }
+        else {
+            var userGuess = event.key.toUpperCase();
+            console.log(userGuess);
+            userGuess.textContent = userGuess;
+        }
 }
