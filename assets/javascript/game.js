fa1ea69bf46f7b8663bfcdaac492abb4a9b6cd81
Scout <therearespidersinthisemail@gmail.com>
2019-05-01 18:20:15 -0600

    was able to get the placeholders to appear on screen by moving location of variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,6 @@
 // var winsDiv = document.getElementById("wins")[0];
 // var lossesDiv = document.getElementById("losses")[0];
 // var livesRemainDiv = document.getElementById("livesRemain")[0];
-var guessFilmText = document.getElementById("guessFilm");
 // var messageDiv = document.getElementById("message")[0];
 // var userGuessesDiv = document.getElementById("userGuesses")[0];
 // var userInterface = document.getElementById("userInterface")[0];
@@ -20,15 +19,14 @@ var guessFilmText = document.getElementById("guessFilm");
 
 
 var  films = ["Slither", "The Omen", "The Ring", "The Orphanage", "Halloween", "The Exorcist", "Trick r Treat", "Phantasm", "The Evil Dead", "Cube", "Hellraiser", "Candyman", "Scream", "Saw", "Us", "IT", "Pet Sematary", "Audition", "The Others", "The Changeling", "The Conjuring", "Poltergeist", "Hausu", "The Shining", "Let the Right One In", "Suspiria", "Cannibal Holocaust", "The Woman in Black", "The Awakening"];
-
 var film = [];
-// var alphabet = Array.from("abcdefghijklmnopqrstuvwxyz");
-
-
-// var wrongGuesses = [];
+var alphabet = Array.from("abcdefghijklmnopqrstuvwxyz");
+var wrongGuesses = [];
 
 
 document.onkeyup = function(event) {
+
+    var guessFilmText = document.getElementById("guessFilm");
     
     var userGuess = event.key.toUpperCase();
 
@@ -45,17 +43,16 @@ document.onkeyup = function(event) {
         film.push(" "); 
 
     } else {
-        film.push("_");  
+        film.push("_"); 
+           
 
 }};
 
+    guessFilmText.textContent = film;
 
-
-
-
-    alert(currentFilm);
+    // alert(currentFilm);
     // currentFilm.textContent = currentFilm;
 
-    console.log(film);
-    film.textContent = film;
+    // console.log(film);
+    // film.textContent = film;
 }
