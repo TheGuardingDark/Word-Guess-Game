7b030cefa05bdc79cb7355bd1f22b085dd0826fe
Scout <therearespidersinthisemail@gmail.com>
2019-05-01 21:46:26 -0600

    wrote block that compares user guess to array and fills in the correct letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,4 @@
 
-
-
-// var isFirstGame = true;
 var gameStarted = false;
 var wins = 0;
 var losses = 0;
@@ -10,6 +7,7 @@ var filmPoster;
 
 var films = ["Slither", "The Omen", "The Ring", "The Orphanage", "Halloween", "The Exorcist", "Trick r Treat", "Phantasm", "The Evil Dead", "Cube", "Hellraiser", "Candyman", "Scream", "Saw", "Us", "IT", "Pet Sematary", "Audition", "The Others", "The Changeling", "The Conjuring", "Poltergeist", "Hausu", "The Shining", "Let the Right One In", "Suspiria", "Cannibal Holocaust", "The Woman in Black", "The Awakening"];
 var film = [];
+var currentFilm = [];
 var alphabet = Array.from("abcdefghijklmnopqrstuvwxyz");
 var wrongGuesses = [];
 
@@ -20,23 +18,17 @@ var userGuessText = document.getElementById("userGuess");
 var holderText = document.getElementById("holder");
 
 
-document.onkeyup = function(event) {
+var startGame = function() {
 
-    if (gameStarted === false) {
-        
-    // isFirstGame = false;
     gameStarted = true;
     livesRemaining = 12;
     randomFilm = [];
     upperCaseFilm = [];
     currentFilm = [];
     wrongGuesses = [];
-    
 
     var guessFilmText = document.getElementById("guessFilm");
     
-    // var userGuess = event.key.toUpperCase();
-
     var randomFilm = films[Math.floor(Math.random() * films.length)];
 
     var upperCaseFilm = randomFilm.toUpperCase();
@@ -53,19 +45,32 @@ document.onkeyup = function(event) {
         film.push("_"); 
     }};
 
-
-
     guessFilmText.textContent = film;
 
-    // alert(currentFilm);
-    // currentFilm.textContent = currentFilm;
+    document.onkeyup = function(event) {
+        if (gameStarted === true) {
 
-    // console.log(film);
-    // film.textContent = film;
-    }
-        else {
             var userGuess = event.key.toUpperCase();
-            console.log(userGuess);
-            userGuess.textContent = userGuess;
+            
+            for (var j = 0; j < currentFilm.length; j++) {
+                if (currentFilm[j] === userGuess) {
+                        film[j] = userGuess;
+                        guessFilmText.textContent = film;
+
+                }
+            }
         }
+    }
 }
+        
+    
+   
+    document.onkeyup = function(event) {
+    
+        if (gameStarted === false) {
+
+            startGame();
+        }}
+
+        
+
