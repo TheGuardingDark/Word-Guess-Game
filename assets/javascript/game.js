487b59d768a06a0f42a468802678ad7418eb3bc2
Scout <therearespidersinthisemail@gmail.com>
2019-05-03 00:51:12 -0600

    created background and added into css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,16 @@ var losses = 0;
 
 var films = ["Slither", "The Omen", "The Ring", "The Orphanage", "Halloween", "The Exorcist", "Trick r Treat", "Phantasm", "The Evil Dead", "Cube", "Hellraiser", "Candyman", "Scream", "Saw", "Us", "IT", "Pet Sematary", "Audition", "The Others", "The Changeling", "The Conjuring", "Poltergeist", "Hausu", "The Shining", "Let the Right One In", "Suspiria", "Cannibal Holocaust", "The Woman in Black", "The Awakening", "The Haunting of Hill House", "The Howling", "The Fly", "The Brood", "The Babadook", "It Follows", "An American Werewolf in London", "Sinister", "Insidious", "Psycho", "The Texas Chainsaw Massacre", "The Silence of the Lambs", "The Descent", "Drag Me to Hell", "The Sixth Sense", "Train to Busan", "Get Out", "The Strangers", "The Wicker Man", "Oculus", "Carrie", "The Cabin in the Woods", "Hostel", "The Eye", "Final Destination", "A Quiet Place", "Martyrs", "The Witch", "Invasion of the Body Snatchers", "Night of the Living Dead", "The Thing", "In the Mouth of Madness"];
 
+
 var alphabet = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+var userGuessText = document.getElementById("userGuess");
+var livesRemainText = document.getElementById("lives");
+var guessFilmText = document.getElementById("guessFilm");
+var lossesText = document.getElementById("losses");
+var winsText = document.getElementById("wins");
+var messageText = document.getElementById("message");
+
 
 
 
@@ -22,7 +31,7 @@ document.onkeyup = function(event) {
 var startGame = function() {
 
     gameStarted = true;
-    lives = 12;
+    lives = 6;
     losses = losses;
     wins = wins;
     randomFilm = [];
@@ -31,12 +40,6 @@ var startGame = function() {
     wrongGuesses = [];
     film = [];
 
-    var userGuessText = document.getElementById("userGuess");
-    var livesRemainText = document.getElementById("lives");
-    var guessFilmText = document.getElementById("guessFilm");
-    var lossesText = document.getElementById("losses");
-    var winsText = document.getElementById("wins");
-    var messageText = document.getElementById("message");
 
     var randomFilm = films[Math.floor(Math.random() * films.length)];
     var upperCaseFilm = randomFilm.toUpperCase();
@@ -97,7 +100,7 @@ var startGame = function() {
                 guessFilmText.textContent = film;
                 wrongGuesses = [];
                 userGuessText.textContent = wrongGuesses;
-                livesRemainText.textContent = 12;
+                livesRemainText.textContent = 6;
                 messageText.textContent = " ";
 
                 startGame();
